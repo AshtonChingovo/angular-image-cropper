737567c51d78e8061923864b558c4d83c8868617
Ashton <ashtonchingovo@gmail.com>
2025-03-30 23:09:11 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageCropperComponent, ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
+
+export type CropperDialogResult = {
+  blob: Blob;
+  imageUrl: string;
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet, ImageCropperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  imagePath = "http://localhost:4200/assets/images/avatar.png";
   title = 'image-cropper';
+  mageCropDialog = false;
+  result: CropperDialogResult | undefined;
+
+  imageCropped(event: ImageCroppedEvent) {
+    const { blob, objectUrl } = event;
+    
+    if (blob && objectUrl) {
+      this.result = { blob, imageUrl: objectUrl };
+      console.log('Cropped image:', this.result.imageUrl);
+    }
+  }
+
+  onCrop() {
+    this.imagePath = this.result?.imageUrl || this.imagePath;
+    console.log('Cropped image:', this.result?.imageUrl);
+    console.log('Cropped image:', this.imagePath);
+  }
+
 }
